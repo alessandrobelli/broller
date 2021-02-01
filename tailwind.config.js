a4899141a8aedae58e2c81f84735113be8134a51
Alessandro Belli <alessandrobelli90@gmail.com>
2021-02-01 14:07:15 +0100

    Initial Commit
    * Simple UI to search, video per search  and video resolution.
    * download button is still in the video interface.
    * make a better UI to search for videos.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      pointerEvents: ['hover', 'focus', 'click'],
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
